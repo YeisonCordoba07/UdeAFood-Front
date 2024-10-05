ad4edbd6204e3ab09f7d5125fe5dc68721898e47
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-10-05 18:23:21 -0500

    Consutar productos por tipo de tienda

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -8,15 +8,21 @@ import Link from 'next/link';
 const Navbar = () => {
 
     const [palabra, setPalabra] = useState("");
+    const [tipoTienda, setTipoTienda] = useState("TODAS");
 
     const router = useRouter();
 
     const { setData } = useBusquedaContext();
 
 
+
+
     const buscarProducto = (e) => {
         e.preventDefault();
-        fetch(`http://localhost:8080/Producto/consultar/${palabra}`, {
+
+        /* Consultar todos los productos
+        "http://localhost:8080/Producto/consultar/"*/
+        fetch(`http://localhost:8080/Seccion/tipo/${tipoTienda}/${palabra}`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -27,8 +33,9 @@ const Navbar = () => {
                 return res.json();
             })
             .then((response) => {
-                console.log(response)
+                //console.log(response)
                 setData(response);
+                console.log("DATA", response.productos);
                 router.push("/busquedaProducto");
 
             })
@@ -41,8 +48,9 @@ const Navbar = () => {
     }
 
 
-    return (
 
+
+    return (
         <nav className='flex flex-[1] justify-between w-full items-center'>
             <div className="flex bg-gray-200 rounded-full  w-full max-w-xl mx-auto py-3 ">
                 <div className="pointer-events-none flex items-center justify-center px-2">
@@ -51,7 +59,6 @@ const Navbar = () => {
 
                 {/* Search Input */}
                 <form onSubmit={buscarProducto} className="flex items-center">
-
                     <InputBase
                         placeholder="Search…"
                         inputProps={{ 'aria-label': 'search' }}
@@ -59,44 +66,49 @@ const Navbar = () => {
                         value={palabra}
                         onChange={(e) => setPalabra(e.target.value)}
                     />
-
                 </form>
             </div>
-                <select
-                        id="ID"
-                        name="tipoTienda"
-                        //onChange={onChange}
-                        className="bg-green-500 mr-60 px-5 py-2 h-10 rounded-lg text-xl outline-brack w-72" >
-                        <option disabled value="" selected>
-                            Seleccione
-                        </option>
-                            <option key="FORMAL" value="FORMAL">
-                                FORMAL
-                            </option>
-                            <option key="INFORMAL" value="INFORMAL">
-                            INFORMAL
-                        </option>
-                        <option key="TODAS" value="TODAS">
-                                TODAS
-                            </option>
-                    
-                    </select>
-        
+
+            <select
+                id="ID"
+                name="tipoTienda"
+                value={tipoTienda}
+                onChange={(e) => {
+                    setTipoTienda(e.target.value);
+                    console.log("SELECTED",e.target.value);
+                }}
+                className="bg-green-500 mr-60 px-5 py-2 h-10 rounded-lg text-xl outline-brack w-72">
+                {/*<option disabled value="">
+                    Seleccione
+                </option>*/}
+                <option key="TODAS" value="TODAS">
+                    TODAS
+                </option>
+                <option key="FORMAL" value="FORMAL">
+                    FORMAL
+                </option>
+                <option key="INFORMAL" value="INFORMAL">
+                    INFORMAL
+                </option>
+
+            </select>
+
             <Link href="/registro">
                 <button
                     type="submit"
                     className="bg-green-600 text-white font-medium text-xl py-4 px-6 rounded-lg hover:bg-green-600 hover:scale-95 duration-300">
-                    Registrarse</button>
+                    Registrarse
+                </button>
             </Link>
             <Link href="/inicioSesion">
                 <button
                     type="submit"
                     className="bg-green-600 text-white font-medium text-xl py-4 px-6 rounded-lg hover:bg-green-600 hover:scale-95 duration-300">
-                    Iniciar sesión </button>
+                    Iniciar sesión
+                </button>
             </Link>
         </nav>
-
-    )
+    );
 }
 
 export { Navbar };
