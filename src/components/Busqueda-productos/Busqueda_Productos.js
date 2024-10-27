f73f920b3b93c745bc10a475ab88ff2515d5e475
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-10-27 15:55:36 -0500

    Actualiza estilos de ordenamiento y categorias

--- src/components/Busqueda-productos/Busqueda_Productos.js
+++ src/components/Busqueda-productos/Busqueda_Productos.js
@@ -98,7 +98,7 @@ const Busqueda_Productos = () => {
             <Categorias />
 
             {/* Selectores para ordenar */}
-            <div className="p-5 text-center space-x-4 flex items-center justify-center">
+            <div className="p-3 text-center space-x-4 flex items-center justify-center border-b border-neutral-200">
                 <label className="text-gray-700 font-semibold mr-2">Ordenar por:</label>
                 <select
                     onChange={(e) => { setSortField(e.target.value); console.log("datos", data); }}
