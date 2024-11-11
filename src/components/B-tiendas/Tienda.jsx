48b29dbf642e4668c095f4692bad510d3444718c
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-11-10 22:25:18 -0500

    Actualiza estilos de los botones del inicio

--- src/components/B-tiendas/Tienda.jsx
+++ src/components/B-tiendas/Tienda.jsx
@@ -1,54 +1,73 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 const Tienda = () => {
 
-    const [openTiendaDialog, setOpenTiendaDialog] = useState(false);
 
     return (
-        <div className="h-96 flex items-center justify-center">
-            <div className="w-full  flex items-center justify-center">
-                <div className="grid grid-cols-3 gap-4 w-1/2">
+        <div className="w-full h-3/5 flex items-center justify-center p-5">
+                <div className="grid grid-cols-3 gap-4 w-[1200px] h-60">
 
                     <Link href='/BusquedaTiendaF'>
                         <button
-                            className="relative flex flex-col items-center justify-center w-full h-full bg-gray-100 shadow-lg transition-colors duration-300 ease-in-out hover:bg-gray-200"
-                            onClick={() => console.log('Ver tiendas formales')}
+                            className="relative flex flex-col items-center justify-center w-full h-full bg-gray-100 shadow-lg transition-colors duration-300 ease-in-out hover:bg-gray-200 rounded-lg hover:brightness-110"
                         >
-                            <img src="formal.jpg" alt="Ver tiendas formales" className="w-full h-full object-cover" />
-                            <div className="absolute bottom-0 w-full text-center bg-gradient-to-t from-gray-900 to-transparent text-white p-2">
-                                <p className="mb-1">Ver</p>
-                                <p>Tiendas formales</p>
+                            <Image src="/Formales.png" alt="Ver tiendas formales"
+                                fill={true} />
+
+                            <div className="absolute bottom-0 w-full text-center bg-gradient-to-t from-gray-900 to-transparent text-white p-2 rounded-b-lg flex flex-col items-start">
+
+                            <p className="text-3xl font-black text-start">
+                                    <small className="text-2xl">
+                                        Ver
+                                    </small><br/>tiendas formales
+                                </p>
+
                             </div>
+   
                         </button>
                     </Link>
                     <Link href='/BusquedaTInformales'>
                         <button
-                            className="relative flex flex-col items-center justify-center w-full h-full bg-gray-100 shadow-lg transition-colors duration-300 ease-in-out hover:bg-gray-200"
-                            onClick={() => console.log('Ver tiendas informales')}
+                            className="relative flex flex-col items-center justify-center w-full h-full bg-gray-100 shadow-lg transition-colors duration-300 ease-in-out hover:bg-gray-200 hover:brightness-110"
                         >
-                            <img src="informal.jpg" alt="Ver tiendas informales" className="w-full h-full object-cover" />
-                            <div className="absolute bottom-0 w-full text-center bg-gradient-to-t from-gray-900 to-transparent text-white p-2">
-                                <p className="mb-1">Ver</p>
-                                <p>Tiendas informales</p>
+
+                            <Image src="/Informales.png" alt="Ver tiendas formales"
+                                fill={true} />
+
+                            <div className="absolute bottom-0 w-full text-center bg-gradient-to-t from-gray-900 to-transparent text-white p-2 rounded-b-lg flex flex-col items-start">
+
+                            <p className="text-3xl font-black text-start">
+                                    <small className="text-2xl">
+                                        Ver
+                                    </small><br/>todas informales
+                                </p>
+
                             </div>
                         </button>
                     </Link>
                     <Link href='BusquedaTodasTiendas'>
                         <button
-                            className="relative flex flex-col items-center justify-center w-full h-full bg-gray-100 shadow-lg transition-colors duration-300 ease-in-out hover:bg-gray-200"
-                            onClick={() => console.log('Ver todas')}
+                            className="relative flex flex-col items-center justify-center w-full h-full bg-gray-100 shadow-lg transition-colors duration-300 ease-in-out hover:bg-gray-200 hover:brightness-110"
                         >
-                            <img src="all.jpg" alt="Ver todas" className="w-full h-full object-cover" />
-                            <div className="absolute bottom-0 w-full text-center bg-gradient-to-t from-gray-900 to-transparent text-white p-2">
-                                <p className="mb-1">Ver</p>
-                                <p>Todas</p>
+
+                            <Image src="/Todas.png" alt="Ver tiendas formales"
+                                fill={true} />
+
+                            <div className="absolute bottom-0 w-full text-center bg-gradient-to-t from-gray-900 to-transparent text-white p-2 rounded-b-lg flex flex-col items-start">
+
+                                <p className="text-3xl font-black text-start">
+                                    <small className="text-2xl">
+                                        Ver
+                                    </small><br/>todas
+                                </p>
+
                             </div>
                         </button>
                     </Link>
                 </div>
 
             </div>
-        </div>
 
     );
 };
